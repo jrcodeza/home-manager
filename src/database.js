e87cc1dbc62ed7dfc4e36ded907029f3b3c7c42d
Jakub Remenec <jremenec@davincisoftware.sk>
2020-03-08 13:37:32 +0100

    Fix index.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ var Mongoose = require('mongoose');
 // load database
 //Mongoose.connect('mongodb://localhost:27017/recipe-manager');
 console.info('Connecting to mongodb on url=' + process.env.MONGODB_URL);
-Mongoose.connect(process.env.MONGODB_URL);
+Mongoose.connect('mongodb+srv://oas-manager-server:K0Yq5VEpZpVdzZMN@recipe-manager-5b3y2.gcp.mongodb.net/test?retryWrites=true&w=majority');
 
 var db = Mongoose.connection;
 
